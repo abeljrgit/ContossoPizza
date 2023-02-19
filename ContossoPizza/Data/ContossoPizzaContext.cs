8414c4411d8f3423260e956b51db87ef932a6ad4
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-19 10:43:05 +0800

    Add project files.

---
+++ ContossoPizza/Data/ContossoPizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using ContossoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContossoPizza.Data
+{
+    public class ContossoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=ContossoPizzaDb;Trusted_Connection=True;Integrated Security=True;");
+        }
+    }
+}
